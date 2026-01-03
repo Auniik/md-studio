b94f6571fa2d1956644defe5793556f2d66ce801
Anik Datta <auniikq@gmail.com>
2026-01-03 15:37:46 +0600

    Removed unused files

--- md_studio/utils/__init__.py
+++ md_studio/utils/__init__.py
@@ -1,7 +1,5 @@
 from .excerpt import create_excerpt
 from .slug import slugify, ensure_unique_slug
-from .toc import extract_table_of_contents, TocItem, HeadingIdGenerator
-from .date_utils import format_relative_date, calculate_reading_time
 from .schemas import (
     CreateDocumentSchema,
     UpdateDocumentSchema,
@@ -16,11 +14,6 @@ __all__ = [
     "create_excerpt",
     "slugify",
     "ensure_unique_slug",
-    "extract_table_of_contents",
-    "TocItem",
-    "HeadingIdGenerator",
-    "format_relative_date",
-    "calculate_reading_time",
     "CreateDocumentSchema",
     "UpdateDocumentSchema",
     "ImportDocumentSchema",
